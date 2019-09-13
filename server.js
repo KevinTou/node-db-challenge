50ef8e978ea13249b88532ee1ac7b2d46a0109d8
Kevin Tou <toukevin@gmail.com>
2019-09-13 11:18:46 -0700

    Finished MVP

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const server = express();
 
 server.use(express.json());
 
-// server.use('/api/projects', projectRouter);
+server.use('/api/projects', projectRouter);
 
 server.get('/', (req, res) => {
   res.send('Server is working!');
